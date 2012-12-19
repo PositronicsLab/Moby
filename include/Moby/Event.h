f4cafe76d1b835afd9445e0046c5a478b349e3b8
drum <drum@drum-VirtualBox.(none)>
2012-12-18 20:50:54 -0500

    Added timing output to driver; contact reduction is now more complete

--- include/Moby/Event.h
+++ include/Moby/Event.h
@@ -130,8 +130,8 @@ class Event
     template <class BidirectionalIterator>
     static void insertion_sort(BidirectionalIterator begin, BidirectionalIterator end);
     static void compute_contact_jacobians(const Event& e, VectorN& Nc, VectorN& Dc1, VectorN& Dc2);
-    static void compute_contact_jacobian(const Event& e, MatrixN& Nc, MatrixN& iM_NcT, unsigned ci, const std::map<DynamicBodyPtr, unsigned>& gc_indices);
-    static void redundant_contacts(const MatrixN& Jc, const MatrixN& Dc, std::vector<unsigned>& nr_indices);
+    static void compute_contact_jacobian(const Event& e, MatrixN& Nc, MatrixN& iM_NcT, MatrixN& iM_DcT, unsigned ci, const std::map<DynamicBodyPtr, unsigned>& gc_indices);
+    static void redundant_contacts(const MatrixN& N, const VectorN& Nv, std::vector<unsigned>& nr_indices);
     static void determine_minimal_subset(std::list<Event*>& group);
 }; // end class
 
