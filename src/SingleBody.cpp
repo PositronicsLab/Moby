fa19b738e3f50c0c0f4f4f83b18595a0c2f8e728
Evan Drumwright <drum@PR2Lab-1.(none)>
2013-03-24 14:15:56 -0400

    Minor updates to facilitate getting articulated bodies

---
+++ src/SingleBody.cpp
@@ -0,0 +1,33 @@
+/****************************************************************************
+ * Copyright 2013 Evan Drumwright
+ * This library is distributed under the terms of the GNU Lesser General Public 
+ * License (found in COPYING).
+ ****************************************************************************/
+
+#include <Moby/ArticulatedBody.h>
+#include <Moby/SingleBody.h>
+
+using namespace Moby;
+
+/// Gets the "super" body
+DynamicBodyPtr SingleBody::get_super_body() const
+{
+/*
+  boost::shared_ptr<const ArticulatedBody> abc = get_articulated_body();
+  if (abc)
+  {
+    ArticulatedBodyPtr ab = boost::const_pointer_cast<ArticulatedBody>(abc);
+    return boost::dynamic_pointer_cast<DynamicBody>(ab);
+  }
+*/
+  ArticulatedBodyPtr ab = get_articulated_body();
+  if (ab)
+    return boost::dynamic_pointer_cast<DynamicBody>(ab);
+  else
+  {
+    BasePtr base = boost::const_pointer_cast<Base>(shared_from_this());
+    return boost::dynamic_pointer_cast<DynamicBody>(base);
+  }
+}
+
+
