50373f57a14ee7d2d8191eca53ac1d9833b23b1c
Evan Drumwright <drum@gwu.edu>
2013-06-28 22:28:29 -0400

    Added LCP solver

--- include/Moby/LCP.h
+++ include/Moby/LCP.h
@@ -15,9 +15,26 @@ namespace Moby {
 class LCP
 {
   public:
+    LCP();
     bool lcp_lemke_regularized(const Ravelin::MatrixNd& M, const Ravelin::VectorNd& q, Ravelin::VectorNd& z, int min_exp = -20, unsigned step_exp = 4, int max_exp = 20, double piv_tol = -1.0, double zero_tol = -1.0);
 bool lcp_lemke(const Ravelin::MatrixNd& M, const Ravelin::VectorNd& q, Ravelin::VectorNd& z, double piv_tol = -1.0, double zero_tol = -1.0);
     bool lcp_lemke(const Ravelin::SparseMatrixNd& M, const Ravelin::VectorNd& q, Ravelin::VectorNd& z, double piv_tol = -1.0, double zero_tol = -1.0);
+
+  private:
+    // temporaries for regularized solver
+    Ravelin::MatrixNd _MMx, _MM;
+    Ravelin::VectorNd _qqx, _wx, _qq;
+
+    // temporaries for Lemke solver
+    Ravelin::VectorNd _d, _Be, _u, _z0, _x, _dl, _xj, _dj, _wl, _result;
+    Ravelin::MatrixNd _Bl, _Al, _t1, _t2;
+    std::vector<unsigned> _all, _tlist, _bas, _nonbas, _j;
+
+    // temporary for sparse Lemke solver
+    Ravelin::SparseMatrixNd _sBl;
+
+    // linear algebra
+    Ravelin::LinAlgd _LA;
 }; // end class
 
 } // end namespace 
