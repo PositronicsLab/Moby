46a00a1809e54257b0ec48c7eef8e12b26b89e44
Evan Drumwright <drum@gwu.edu>
2013-09-27 12:11:49 -0400

    Converted g/b frames to a/b frames in GeneralizedCCD

--- include/Moby/GeneralizedCCD.h
+++ include/Moby/GeneralizedCCD.h
@@ -65,23 +65,23 @@ class GeneralizedCCD : public CollisionDetection
     // structure passed to determine_TOI
     struct DStruct
     {
-      CollisionGeometryPtr gb; // geometry from which point is taken
-      CollisionGeometryPtr gs; // geometry against which point is tested
+      CollisionGeometryPtr ga; // geometry from which point is taken
+      CollisionGeometryPtr gb; // geometry against which point is tested
 
-      // gs's poses at time t0,tf 
-      Ravelin::Pose3d Ps_t0, Ps_tf;
+      // gb's poses at time t0,tf 
+      Ravelin::Pose3d Pb_t0, Pb_tf;
 
-      // transforms from gb's frame to gs's frame at t0,tf
-      Ravelin::Transform3d sTb_t0, sTb_tf;
+      // transforms from ga's frame to gb's frame at t0,tf
+      Ravelin::Transform3d bTa_t0, bTa_tf;
 
-      // vertex to test for intersection (in gb's frame) 
-      Point3d u_b;     
+      // vertex to test for intersection (in ga's frame) 
+      Point3d u_a;     
 
-      // relative orientations (orientation of gb in gs's frame) at times t0/tf 
+      // relative orientations (orientation of ga in gb's frame) at times t0/tf 
       Ravelin::Quatd q0, qf;      
 
-      // BV corresponding to part of gs
-      BVPtr s_BV;              
+      // BV corresponding to part of gb
+      BVPtr b_BV;              
     };
 
     /// Pair of poses (one at time t0, the other at time tf)
@@ -115,7 +115,7 @@ class GeneralizedCCD : public CollisionDetection
     static double calc_min_dev(const Ravelin::Vector3d& u, const Ravelin::Vector3d& d, const Ravelin::Quatd& q1, const Ravelin::Quatd& q2, double& t);
     static double calc_max_dev(const Ravelin::Vector3d& u, const Ravelin::Vector3d& d, const Ravelin::Quatd& q1, const Ravelin::Quatd& q2, double& t);
     static std::pair<double, double> calc_deviations(const Ravelin::Vector3d& u, const Ravelin::Vector3d& d, const Ravelin::Quatd& q1, const Ravelin::Quatd& q2, double ta, double tb);
-    static void populate_dstruct(DStruct* ds, CollisionGeometryPtr gb, const Ravelin::Pose3d& Pb_t0, const Ravelin::Pose3d& Pb_tf, CollisionGeometryPtr gs, const Ravelin::Pose3d& Ps_t0, const Ravelin::Pose3d& Ps_tf, BVPtr s_BV);
+    static void populate_dstruct(DStruct* ds, CollisionGeometryPtr ga, const Ravelin::Pose3d& Pa_t0, const Ravelin::Pose3d& Pa_tf, CollisionGeometryPtr gb, const Ravelin::Pose3d& Pb_t0, const Ravelin::Pose3d& Pb_tf, BVPtr b_BV);
     void add_rigid_body_model(RigidBodyPtr body);
     double determine_TOI(double t0, double tf, const DStruct* ds, Point3d& pt, Ravelin::Vector3d& normal) const;
     double determine_TOI_fast(double t0, double tf, const DStruct* ds, Point3d& pt, Ravelin::Vector3d& normal) const;
