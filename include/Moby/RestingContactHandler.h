5de0a332133b559540220ba20b8b00b10d012a39
samzapo <samzapo@Giskard.(none)>
2013-10-02 10:56:08 -0400

    Fixed typo in Resting Contact LCP set-up

--- include/Moby/RestingContactHandler.h
+++ include/Moby/RestingContactHandler.h
@@ -37,16 +37,6 @@ class RestingContactHandler
 }; // end class
 
 /// Exception thrown when trying to initialize a fixed size vector/matrix with the wrong size
-//class EnergyToleranceException : public std::runtime_error
-//{
-//  public:
-//    EnergyToleranceException(const std::list<Event*>& contact_events) : std::runtime_error("post-event Kinetic Energy exceeds pre-event Kinetic Energy!") { events = contact_events; }
-
-//    virtual ~EnergyToleranceException() throw() { }
-
-//  std::list<Event*> events;
-//}; // end class
-
 class RestingContactFailException : public std::runtime_error
 {
   public:
