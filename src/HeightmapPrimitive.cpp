2bd38ca357b4281181cd5239c24328c9af0f4b79
Evan Drumwright <drum@gwu.edu>
2014-05-29 22:49:47 -0500

    Removed heightmap height=0 debugging code

--- src/HeightmapPrimitive.cpp
+++ src/HeightmapPrimitive.cpp
@@ -501,7 +501,6 @@ void HeightmapPrimitive::load_from_xml(shared_ptr<const XMLTree> node, std::map<
       _heights.set_zero(1,1);
     }
   }
-_heights *= 0.0;
 
   // read in the width, if specified
   XMLAttrib* width_attr = node->get_attrib("width");
