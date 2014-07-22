7d6c22c9ad1cf519b24fbaaffdf65319d0bb7d46
drum <drum@dirac-VirtualBox.(none)>
2014-07-21 21:04:44 -0500

    Changed to Apache v2.0 license

--- include/Moby/XMLReader.h
+++ include/Moby/XMLReader.h
@@ -1,7 +1,7 @@
 /****************************************************************************
  * Copyright 2005 Evan Drumwright
- * This library is distributed under the terms of the GNU Lesser General Public 
- * License (found in COPYING).
+ * This library is distributed under the terms of the Apache V2.0 
+ * License (obtainable from http://www.apache.org/licenses/LICENSE-2.0).
  ****************************************************************************/
 
 #ifndef _XML_READER_H
